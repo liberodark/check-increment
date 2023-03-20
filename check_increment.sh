8d211e6771450c4386920f68df2ee04425fcd9d0
liberodark <liberodark@gmail.com>
2023-03-20 10:58:32 +0100

    Update check_increment.sh

--- check_increment.sh
+++ check_increment.sh
@@ -5,36 +5,158 @@
 # Thanks : lord, play-cool-friend
 # License: GNU GPLv3
 
-version="0.0.1"
+version="0.0.4"
 
 echo "Welcome on Check Acces File $version"
-
-#=================================================
-# CHECK ROOT
-#=================================================
-
-if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 #=================================================
 # RETRIEVE ARGUMENTS FROM THE MANIFEST AND VAR
 #=================================================
 
-#distribution=$(cat /etc/*release | grep "PRETTY_NAME" | sed 's/PRETTY_NAME=//g' | sed 's/["]//g' | awk '{print $1}')
-mail="my.email@domain.fr"
-check_folder_1=$(find /Data/365/* -mmin +120)
-check_folder_2=$(find /Data/90/* -mmin +120)
+mail="my.email@domain.fr my.email@domain.com"
+check_folder_1="/Data/365"
+check_folder_2="/Data/90"
+check_service=$(systemctl list-units --state=failed | grep rsyslog)
+rsyslog_name="fr-ren-rsyslog"
+duration="60"
 
-
-if [ -z "$check_folder_1" ]; then
-echo "Files would be OK"
-else
-echo "Error files not be modified in 2h"
-echo "$check_folder_1" | mail -s "TEST KO" -r "my_domain" "$mail"
+# Check Folders
+if [ ! -d "$check_folder_1" ]; then
+echo "$check_folder_1 does not exists.." | mail -s "TEST KO" -r "$rsyslog_name" "$mail"
+exit
 fi
 
-if [ -z "$check_folder_2" ]; then
-echo "Files would be OK"
-else
-echo "Error files not be modified in 2h"
-echo "$check_folder_2" | mail -s "TEST KO" -r "my_domain" "$mail"
+if [ ! -d "$check_folder_2" ]; then
+echo "$check_folder_2 does not exists.." | mail -s "TEST KO" -r "$rsyslog_name" "$mail"
+exit
 fi
+
+# Check Service
+if [ -z "$check_service" ]; then
+echo "Service is OK"
+else
+echo "$check_service service is failed.." | mail -s "TEST KO" -r "$rsyslog_name" "$mail"
+exit
+fi
+
+usage ()
+{
+     echo "usage: check_increments.sh -switch"
+     echo "options:"
+     echo "-switch: for check switch"
+     echo "-server: for check servers"
+     echo "-firewall: for check bastion"
+     echo "-h: Show help"
+}
+
+check_switch(){
+#==============================================
+# SWITCH
+#==============================================
+echo "Check Switchs"
+
+switch_list="sw00
+sw01"
+
+for switch in $switch_list
+    do
+        if [ ! -e "${check_folder_2}/${switch}".log ]; then
+            echo "${switch}.log does not exists.." | mail -s "TEST KO" -r "$rsyslog_name" "$mail"
+    else
+        command=$(find "${check_folder_2}/${switch}".log -mmin +${duration})
+        if [ -z "$command" ]; then
+            echo "Files would be OK" > /dev/null 2>&1
+        
+    else
+            echo "$command is not incremented" | mail -s "TEST KO" -r "$rsyslog_name" "$mail"
+    fi
+fi
+done
+}
+
+check_servers(){
+#==============================================
+# SERVERS CLASSE C
+#==============================================
+echo "Check Servers"
+
+server_list="server00
+server01"
+
+for server in $server_list
+    do
+        if [ ! -e "${check_folder_2}/${server}".log ]; then
+            echo "${server}.log does not exists.." | mail -s "TEST KO" -r "$rsyslog_name" "$mail"
+    else
+        command=$(find "${check_folder_2}/${server}".log -mmin +${duration})
+        if [ -z "$command" ]; then
+            echo "Files would be OK" > /dev/null 2>&1
+        
+    else
+            echo "$command is not incremented" | mail -s "TEST KO" -r "$rsyslog_name" "$mail"
+    fi
+fi
+done
+}
+
+check_firewall(){
+#==============================================
+# FIREWALL
+#==============================================
+echo "Check Firewall"
+
+fw_list="fw00
+fw01"
+
+for fw in $fw_list
+    do
+        if [ ! -e "${check_folder_1}/${fw}".log ]; then
+            echo "${fw}.log does not exists.." | mail -s "TEST KO" -r "$rsyslog_name" "$mail"
+    else
+        command=$(find "${check_folder_1}/${fw}".log -mmin +${duration})
+        if [ -z "$command" ]; then
+            echo "Files would be OK" > /dev/null 2>&1
+        
+    else
+            echo "$command is not incremented" | mail -s "TEST KO" -r "$rsyslog_name" "$mail"
+    fi
+fi
+done
+}
+
+parse_args ()
+{
+    while [ $# -ne 0 ]
+    do
+        case "${1}" in
+            -switch)
+                shift
+                check_switch >&2
+                return 0
+                ;;
+            -server)
+                shift
+                check_servers >&2
+                return 0
+                ;;
+            -firewall)
+                shift
+                check_firewall >&2
+                return 0
+                ;;
+            -h|--help)
+                usage
+                exit 0
+                ;;
+            *)
+                echo "Invalid argument : ${1}" >&2
+                usage >&2
+                exit 1
+                ;;
+        esac
+        shift
+    done
+
+}
+
+parse_args "$@"
